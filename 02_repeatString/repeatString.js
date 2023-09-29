6e91533abbed95d446f9d27b54d99e6500d872aa
ddoabdullahXubuntu <mfirdaus.tch@gmail.com>
2023-09-29 20:09:05 +0800

    Finish exe 1 and 2
    Managed to pass all the test required.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    //(typeof toPrint == 'string')? :return("its not a string");
+    let repeatedString = '';
+    if(num<0)return "ERROR";
+    for (let i = 1; i<=num;i++){
+        if(string == '') return '';
+        repeatedString=repeatedString.concat(string);
+    }
+    return repeatedString;
 };
 
+console.log(repeatString('hey',3));
 // Do not edit below this line
 module.exports = repeatString;
