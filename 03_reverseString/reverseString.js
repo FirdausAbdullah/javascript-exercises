77a73845b24bf1d82ccd0c31b613a2a32d399768
ddoabdullahXubuntu <mfirdaus.tch@gmail.com>
2023-09-29 20:23:09 +0800

    Finish reverseString exercise
    Passed the exercise's test.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed;
+    return reversed = string.split("").reverse().join("");
 };
-
 // Do not edit below this line
 module.exports = reverseString;
