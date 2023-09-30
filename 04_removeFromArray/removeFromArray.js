d13bb3fb8522ca45ac7fd07b756326c1bd10c36c
ddoabdullahXubuntu <mfirdaus.tch@gmail.com>
2023-09-30 19:50:17 +0800

    Finish exercise 4 removeFromArray
    Manage to pass all test for this exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(testArray, ...toRemove) {
+    
+    let removeArray = Array.from(toRemove);
+    let newArray = [];
+    let equal;
+    for(let i =0; i<testArray.length;i++){
+       
+        for(let j=0 ; j<toRemove.length; j++){
+            
+            if (testArray[i]===toRemove[j]){
+                equal = 1;
+            }
 
+        }
+        if (equal == 1){equal=0;}
+        else{
+            
+            newArray.push(testArray[i]);
+            
+        }
+    }
+
+    return newArray;
 };
+
+//removeFromArray([1,2,3,4],2,3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
